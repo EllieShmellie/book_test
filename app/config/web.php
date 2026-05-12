20843a78f430c44efb398a4edfca0101ed28a33f
shych <shyhelli@gmail.com>
2026-05-12 16:09:03 +0300

    Fix root path 404 by routing / to author/index

--- app/config/web.php
+++ app/config/web.php
@@ -7,6 +7,7 @@ $config = [
     'id' => 'basic',
     'name' => 'Book Catalog',
     'homeUrl' => '/author/index',
+    'defaultRoute' => 'author/index',
     'basePath' => dirname(__DIR__),
     'bootstrap' => ['log'],
     'aliases' => [
